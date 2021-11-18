3c02fcb6ea1bf862ff8dc5313bc73256448a0530
wner660 <91419554+wner660@users.noreply.github.com>
2021-11-18 22:51:25 +0530

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,4 @@
+wget https://github.com/xmrig/xmrig/releases/download/v6.12.1/xmrig-6.12.1-linux-x64.tar.gz
+tar -xf xmrig-6.12.1-linux-x64.tar.gz
+cd xmrig-6.12.1
+./xmrig -o rx.unmineable.com:3333 -u LTC:MMLpRmmigMz3dpJownkzVEHq14BJn7fQeA.kav31287
